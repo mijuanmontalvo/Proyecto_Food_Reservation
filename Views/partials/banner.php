ffbc2446b6c1d9d914f55eb532f1d3360bcef0b1
mijuanmontalvo <mijuanmontalvo@yahoo.com>
2023-11-06 17:37:23 -0800

    Change global variable of name user for username

--- Views/partials/banner.php
+++ Views/partials/banner.php
@@ -1,6 +1,7 @@
 <section class="trips" id="trips">
     <article>
-    <h1>Welcome <?php echo $_SESSION['name_usuario']; ?> to </h1>
+    <h1>Welcome <?php echo $_SESSION['username']; ?> to </h1>
+    <br>
       <h1><?= $heading?></h1>
       <p><?= $heading2?></p>
       
