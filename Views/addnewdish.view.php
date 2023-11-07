aee9d66c4cd6da88cf7661dee788fe409fc6c3cd
mijuanmontalvo <mijuanmontalvo@yahoo.com>
2023-11-06 17:34:05 -0800

    Create files php for insert new dishes in database

--- Views/addnewdish.view.php
+++ Views/addnewdish.view.php
@@ -6,12 +6,26 @@
   <!-- Trips -->
 <?php require('partials/banner.php')?> 
 <section class="gallery" >
-        <li>
-            <a href="/addnewdish2" class="text-blue-500 hover:underline">
-            Ingrese los datos del nuevo plato
-            </a>
-        </li>
-
+        <h2>
+        Enter the data of the new plate
+            
+        </h2>
+        <hr>
+        <form action="/insertnewdish" method="post" enctype="multipart/form-data">
+        Dish Name:<br>
+        <input type="text" name="name">
+        <br>
+        Description:<br>
+        <textarea name="description" rows="4" cols="50"></textarea>
+        <br><br>
+        Price:<br>
+        <input type="text" name="price">
+        <br><br>
+        Dish image:<br>
+        <input type="file" name="image_dish">
+        <br><br>
+        <input type="submit" value="Add dish">
+        </form>
 
   </section>
 
---
+++ controllers/insertnewdish.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+include '../functions.php';
+$conn = conectarBD();
+
+$name = $_POST['name'];
+$description = $_POST['description'];
+$price = $_POST['price'];
+
+
+$image_dish = addslashes(file_get_contents($_FILES['image_dish']['tmp_name']));
+
+// Ejecutar una consulta (query) simple
+$sql = "INSERT INTO food_reservation.dish (Name, Description, Price, Image) 
+        values('$name','$description','$price','$image_dish')";
+
+if ($conn->query($sql) === TRUE) {
+    // Redireccionar a la página de inicio de sesión
+
+    header("Location: /manageourdishes");
+    echo "exito";
+    exit();
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+$conn->close();
+?>
