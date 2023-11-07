281ec55912133e188b0184fdf367c0ad96171964
mijuanmontalvo <mijuanmontalvo@yahoo.com>
2023-11-07 15:19:31 -0800

    Update li in home page

--- Views/home.view.php
+++ Views/home.view.php
@@ -6,17 +6,23 @@
   <!-- Trips -->
 <?php require('partials/banner.php')?> 
 <section class="gallery" >
-        <li>
-            <a href="/ourdishes" class="text-blue-500 hover:underline">
-                Our Dishes
-            </a>
-        </li>       
-        <li>
-            <a href="/reservation" class="text-blue-500 hover:underline">
-                Reservations
-            </a>
-        </li>
 
+
+        <ul class="tripList trips">
+      <li>
+        
+        <h3>Our Dishes</h3>
+        <p>Here you can check our dishes</p>
+        <a href="/ourdishes">Continue...<i class="fas fa-angle-double-right"></i></a>
+      </li>
+      <li>
+        
+        <h3>Reservations</h3>
+        <p>Here you can do a reservation and you can review all your reservations.</p>
+        <a href="/reservation">Continue...<i class="fas fa-angle-double-right"></i></a>
+      </li>
+
+    </ul>
 
   </section>
 
