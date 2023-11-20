f8f29c6b5c1ad9c2dda1e1b1cc0abdb9dbf65502
mijuanmontalvo <mijuanmontalvo@yahoo.com>
2023-11-20 12:44:13 -0800

    Update massage of welcome in banner

--- Views/partials/banner.php
+++ Views/partials/banner.php
@@ -1,7 +1,12 @@
 <section class="trips" id="trips">
+<article>
+      
+      <h1>Welcome <?php echo $_SESSION['username'] ?> to </h1>
+      
+      
+      </article>
     <article>
-    <h1>Welcome <?php echo $_SESSION['username']; ?> to </h1>
-    
+   
       <h1><?= $heading?></h1>
       <p><?= $heading2?></p>
       
