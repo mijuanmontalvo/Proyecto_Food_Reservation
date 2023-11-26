2809f93bf364ded36a915c624630d7d6f8a047b7
mijuanmontalvo <mijuanmontalvo@yahoo.com>
2023-11-26 11:04:22 -0800

    Update format of code and add $ in price field

--- Views/ourdishes.view.php
+++ Views/ourdishes.view.php
@@ -8,7 +8,7 @@
 <?php require('partials/banner.php')?> 
 
 
-  <ul class="tripList trips">
+    <ul class="tripList trips">
       <li>
         
         <h3>Review all our dishes</h3>
@@ -28,11 +28,11 @@
 
 
 
-    <section class="gallery" >
+  <section class="gallery" >
 
     <?php
-if ($_SESSION['UserType']=="Kitchen"){
-?>
+    if ($_SESSION['UserType']=="Kitchen"){
+    ?>
             <div class="addnewdish">
             <a href="/addnewdish"  class="text-blue-500 hover:underline boton">
             Add a new dish
@@ -40,79 +40,70 @@ if ($_SESSION['UserType']=="Kitchen"){
             </div> 
 
             <?php
-} else {
+    } else {
 
-}
-?>
+    }
+    ?>
 
         <br>
-<hr>
-<br>
-<h2>Dish Table</h2>
-<hr>
-<table style="width:100%">
-  <tr>
-    <th>ID</th>
-    <th>Name</th>
-    <th>Description</th>
-    <th>Price ($)</th>
-    <th>Image</th>
-    <th>Operations</th>
-  </tr>
- 
-<?php
-// Crear conexión
-include 'db_connect.php';
-//echo $_SESSION['UserType'];
-
-
-
-// Consulta para seleccionar todos los datos de la tabla
-$sql = "SELECT ID, name, description, price, image FROM dish order by ID desc";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // Imprimir los datos en una tabla
-  
-  while($row = $result->fetch_assoc()) {
+    <hr>
+    <br>
+    <h2>Dishes Table</h2>
+    <hr>
+    <table style="width:100%">
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Price ($)</th>
+        <th>Image</th>
+        <th>Operations</th>
+      </tr>
     
-    echo "<tr>";
-    echo "<td>" . $row['ID'] . "</td>";
-    echo "<td>" . $row['name'] . "</td>";
-    echo "<td>" . $row['description'] . "</td>";
-    echo "<td>" . number_format($row['price'],2) . "</td>";
-    echo "<td><img src='data:image/jpeg;base64," . base64_encode($row['image']) . "'/></td>";
+    <?php
+    // Crear conexión
+    include 'db_connect.php';
+    //echo $_SESSION['UserType'];
 
-    if ($_SESSION['UserType']=="Kitchen"){
-    echo "<td>" ." <a href=/editdish?id=$row[ID]>Edit</a> | 
-                   <a href=/deletedish?id=$row[ID] class=table__item__link>Delete</a>" . "</td>";
-    }else{
-      echo "<td>" ." <a href=/editdish?id=$row[ID]>Reserve</a>" . "</td>";
+
+
+    // Consulta para seleccionar todos los datos de la tabla
+    $sql = "SELECT ID, name, description, price, image FROM dish order by ID desc";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+      // Imprimir los datos en una tabla
+      
+      while($row = $result->fetch_assoc()) {
+        
+        echo "<tr>";
+        echo "<td>" . $row['ID'] . "</td>";
+        echo "<td>" . $row['name'] . "</td>";
+        echo "<td>" . $row['description'] . "</td>";
+        echo "<td>" . "$ ".number_format($row['price'],2) . "</td>";
+        echo "<td><img src='data:image/jpeg;base64," . base64_encode($row['image']) . "'/></td>";
+
+        if ($_SESSION['UserType']=="Kitchen"){
+        echo "<td>" ." <a href=/editdish?id=$row[ID]>Edit</a> | 
+                      <a href=/deletedish?id=$row[ID] class=table__item__link>Delete</a>" . "</td>";
+        }else{
+          echo "<td>" ." <a href=/editdish?id=$row[ID]>Reserve</a>" . "</td>";
+        }
+        echo "</tr>";
+
+        
+      }
+    } else {
+      echo "0 resultados";
     }
-    echo "</tr>";
+    $conn->close();
 
-    
-  }
-} else {
-  echo "0 resultados";
-}
-$conn->close();
+    ?>
 
-
-?>
-
-</table>
+    </table>
 
   </section>
 
-
-    
-<?php 
-
-
-
-
-?>
   <!-- Footer -->
 <!-- Contact Info -->
 <?php //require('partials/footer.contact.php')?>
