5f8190e6b0e65ab5dd71ac0c2fc3088470ee467d
mijuanmontalvo <mijuanmontalvo@yahoo.com>
2023-11-28 12:36:43 -0800

    Update link after to insert a reservation

--- controllers/insertar_reservation.php
+++ controllers/insertar_reservation.php
@@ -17,7 +17,7 @@ $sql = "INSERT INTO food_reservation.reservation (UserID, DishID, DateReservatio
 
 if ($conn->query($sql) === TRUE) {
     // Redireccionar a la página de inicio de sesión
-    header("Location: /reservationsmade");
+    header("Location: /reservation");
     exit();
   } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
