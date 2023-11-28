fd6f751b1c42604eb0c7425ff1315ad4eda1155b
mijuanmontalvo <mijuanmontalvo@yahoo.com>
2023-11-28 12:28:23 -0800

    Edit text of home page

--- Views/home.view.php
+++ Views/home.view.php
@@ -9,18 +9,47 @@
 
 
         <ul class="tripList trips">
+        <?php
+        if ($_SESSION['UserType']=="Kitchen"){
+        ?>
+
       <li>
         
         <h3>Our Dishes</h3>
-        <p>Here you can check our dishes</p>
+        <p>In this section of our application you can review the list of our dishes available to guests, you can also create new dishes, edit dishes and/or delete dishes.</p>
         <a href="/ourdishes">Continue...<i class="fas fa-angle-double-right"></i></a>
       </li>
       <li>
         
         <h3>Reservations</h3>
-        <p>Here you can do a reservation and you can review all your reservations.</p>
+        <p>In this section of our application you can review all guest reservations previously made, you can also edit those reservations and/or delete those reservations.</p>
         <a href="/reservation">Continue...<i class="fas fa-angle-double-right"></i></a>
       </li>
+<?php
+        } else {
+
+?>
+      <li>
+        
+        <h3>Our Dishes</h3>
+        <p>In this section of our application you can review the list of dishes at your disposal, you can also select a dish and reserve it so you can enjoy it on your preferred date and time.</p>
+        <a href="/ourdishes">Continue...<i class="fas fa-angle-double-right"></i></a>
+      </li>
+      <li>
+        
+        <h3>Reservations</h3>
+        <p>In this section of our application you can review all your previously made reservations, you can also create new reservations, edit your reservations and/or delete your reservations.</p>
+        <a href="/reservation">Continue...<i class="fas fa-angle-double-right"></i></a>
+      </li>
+
+
+<?php
+        }
+
+?>
+
+
+
 
     </ul>
 
