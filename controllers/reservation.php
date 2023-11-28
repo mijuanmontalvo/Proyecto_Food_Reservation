0b4de845bb8a7284877bb771fe2372a7fefd1cad
mijuanmontalvo <mijuanmontalvo@yahoo.com>
2023-11-28 12:19:11 -0800

    Edit text of page head

--- controllers/reservation.php
+++ controllers/reservation.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 $heading = "Reservation";
-$heading2 = "You jjcan make a reservation for our food or review your reservations already made";
+$heading2 = "You can make a reservation for our food or review your reservations already made";
 
 require "views/reservation.view.php";  
