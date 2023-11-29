44f0676dfffa8c2d723b1f9a959af4fdb27c590e
mijuanmontalvo <mijuanmontalvo@yahoo.com>
2023-11-28 16:10:00 -0800

    Update welcome text in banner

--- Views/partials/banner.php
+++ Views/partials/banner.php
@@ -1,7 +1,7 @@
 <section class="trips" id="trips">
     <article>
       
-      <h2>Welcome <?php echo $_SESSION['username'] ?> (<?php echo $_SESSION['UserType'] ?>) to </h2>
+      <h2>Welcome <?php echo $_SESSION['Name'] ?> (<?php echo $_SESSION['username']." - " ?><?php echo $_SESSION['UserType'] ?>) to </h2>
     </article>
     <article>
    
